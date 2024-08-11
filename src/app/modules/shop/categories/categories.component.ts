ba4d6a15323040a62a6ab2cd5e9526e3fdea0250
SebastianContreras <90106481@grupovanti.com>
2024-08-10 19:35:21 -0500

    feat: install boostrapt

--- src/app/modules/shop/categories/categories.component.ts
+++ src/app/modules/shop/categories/categories.component.ts
@@ -3,8 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-categories',
   templateUrl: './categories.component.html',
-  styleUrls: ['./categories.component.css']
+  styleUrls: ['./categories.component.scss']
 })
+
 export class CategoriesComponent {
 
 }
